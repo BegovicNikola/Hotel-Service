2230a5fd24546db7f9df1374abff94f0e0cae1af
BegovicNikola <begovic.office@gmail.com>
2019-04-24 12:28:12 +0200

    Hotreload Fix (App has to be class component)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { GuestProvider } from './providers/GuestProvider'
 import GuestList from './components/GuestList/GuestList'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
-const App = () => {
-  return (
-    <GuestProvider>
-      <div className="App">
-        <GuestList />
-      </div>
-    </GuestProvider>
-  )
+class App extends Component {
+  render() {
+    return (
+      <GuestProvider>
+        <div className="App">
+          <GuestList />
+        </div>
+      </GuestProvider>
+    )
+  }
 }
 
 export default App
