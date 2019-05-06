4cd93d05da62d507f947af71d92cf3e29b4df3d3
BegovicNikola <begovic.office@gmail.com>
2019-05-06 10:51:17 +0200

    Utilitu Not Found

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { GuestProvider } from './providers/GuestProvider'
 import GuestList from './components/GuestList/GuestList'
 import GuestAdd from './components/GuestAdd/GuestAdd'
+import NotFound from './utils/pages/NotFound'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
@@ -12,8 +13,9 @@ const App = () => {
       <Router>
         <div className="App">
           <Switch>
-            <Route exact path="/add" component={GuestAdd} />
             <Route exact path="/" component={GuestList} />
+            <Route exact path="/add" component={GuestAdd} />
+            <Route component={NotFound} />
           </Switch>
         </div>
       </Router>
