fe4f25de3d6fef1e464fb01dadf57cc2cbd18075
BegovicNikola <begovic.office@gmail.com>
2019-05-06 10:35:51 +0200

    Not Found Routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { GuestProvider } from './providers/GuestProvider'
 import GuestList from './components/GuestList/GuestList'
+import GuestAdd from './components/GuestAdd/GuestAdd'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 const App = () => {
   return (
     <GuestProvider>
-      <div className="App">
-        <GuestList />
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/add" component={GuestAdd} />
+            <Route exact path="/" component={GuestList} />
+          </Switch>
+        </div>
+      </Router>
     </GuestProvider>
   )
 }
---
+++ src/components/GuestAdd/GuestAdd.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GuestAdd = () => {
+  return (
+    <div>
+      <h1>Add</h1>
+    </div>
+  )
+}
+
+export default GuestAdd
